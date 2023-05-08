a0b9f16a7bac161004c17b3bc12a7fb713cbcc81
limanp <pasha.liman@ukr.net>
2023-05-08 21:27:44 +0300

    update laboratory 2

--- LaboratoryWork_02/Task_08.py
+++ LaboratoryWork_02/Task_08.py
@@ -1,3 +1,14 @@
 
 # Task_08 -----------------------------------------
 
+print("Введіть три числа: ")
+numbers = [int(input()), int(input()), int(input())]
+count = 0
+
+i = 0
+while(i < len(numbers)):
+    if(numbers[i] > 0):
+        count += 1
+    i += 1
+
+print("Кількість позитивних чисел:", count)
