ec7ba7198702077b6d7514c16106c9cb7615ed9b
limanp <pasha.liman@ukr.net>
2023-05-11 07:49:01 +0300

    Laboratory_02 Task 14

---
+++ LaboratoryWork_02/Task_14.py
@@ -0,0 +1,21 @@
+
+# Task_14 -----------------------------------------
+
+import math
+
+print("Введіть число [N] (N > 1): ")
+N = int(input())
+
+if N > 1:
+    K = 1
+    minK = 1
+    while K < N:
+        if math.pow(5, K) > N:
+            minK = K
+            break
+        K += 1
+    print("Мінімальне число [K] при умові 5 ^ K > N =", minK)
+else:
+    print("Помилка!!! Введіть число [N] яке більше 1")
+
+
