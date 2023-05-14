9a8cf3d26429e40b6485a0a00c8c71087473e0bb
limanp <pasha.liman@ukr.net>
2023-05-14 09:40:33 +0300

    Laboratory_06 Task 1

--- LaboratoryWork_06/Task_01/Task_01.py
+++ LaboratoryWork_06/Task_01/Task_01.py
@@ -8,7 +8,10 @@ if os.path.exists('numbers.txt'):
             sum += int(number)
         except:
             print("Помилка! У файлі є символи, що не являються числами")
+    myFile.close()
     print(sum)
+    myFile = open('sum_numbers.txt', 'w')
+    myFile.write(f"Сума чисел з файлу numbers.txt = {sum}")
     myFile.close()
 else:
     print("Помилка!!! Файлу не існує")
