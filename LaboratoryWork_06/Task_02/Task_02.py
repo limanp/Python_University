5dc4f73d79b7c4a2432abae46e68f2922bba770c
limanp <pasha.liman@ukr.net>
2023-05-14 09:54:52 +0300

    Laboratory_06 Task 2

---
+++ LaboratoryWork_06/Task_02/Task_02.py
@@ -0,0 +1,12 @@
+
+print("Введіть числа через пробіл: ")
+numbers = list(map(int, input().split()))
+
+myFile = open('result.txt', 'w')
+
+for i in numbers:
+    if i % 2 == 0:
+        myFile.write(f"Число {i} парне\n")
+    else:
+        myFile.write(f"число {i} не парне\n")
+myFile.close()
