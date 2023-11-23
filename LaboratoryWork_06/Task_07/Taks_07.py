d73abade6eb67c7d4ddf299c3b4cfd7e79b4b9dd
limanp <pasha.liman@ukr.net>
2023-11-23 10:37:49 +0200

    update Lab_06 Task_07

--- LaboratoryWork_06/Task_07/Taks_07.py
+++ LaboratoryWork_06/Task_07/Taks_07.py
@@ -11,20 +11,53 @@ def checkRepeatGrade(gradeCheck, grades):
             count = count + 1
     return count
 
-countStudent = 0
+
+
 with open('marks.lab6.csv', encoding='UTF-8') as fileMarks:
-    studentGrades = []
+    studentsGrades = []
+    countStudent = 0
+    averageGrades = []
+    statsStudentResult = []
     for entry in fileMarks:
         student = entry.split(',', 4)
 
-        # print(student[4].replace('\",\"', " ").replace('\"', "").replace(',-', ' ').split())
-        grade = student[4].replace('\",\"', " ").replace('\"', "").replace(',-', ' ').split()[0].replace(',', '.')
-        # print(grade)
-        studentGrades.append(grade)
+        resultTest = student[4].replace('\",\"', " ").replace('\"', "").replace(',-', ' ').split()
+        grade = resultTest[0].replace(',', '.')
+        studentsGrades.append(grade)
 
+        leadTime = student[3].replace(' хв ', '.').replace(' сек', '')
+
+        # Розрахунок середньої оцінки с кроком 1 хвилина
+        averageGrades.append(float(grade) / (len(resultTest) - 1))
+
+        # Лічильник студентів
         countStudent += 1
 
-print(f'Кількість студентів що проходили тестування: {countStudent}')
+        i = 1
+        countCorrectAnswer = 0
+        while i < len(resultTest):
+            if resultTest[i] == "0,50":
+                countCorrectAnswer = countCorrectAnswer + 1
+            i = i + 1
+        statsStudentResult.append(countCorrectAnswer)
 
-for grade in range(1, 11):
-    print(f"Оцінку {grade} отримало = {checkRepeatGrade(grade, studentGrades)}")
+
+print(f'Кількість студентів що проходили тестування: {countStudent}\n')
+print(student)
+print(statsStudentResult)
+
+grade = 0
+while(grade <= 10):
+    print(f"Оцінку {grade} отримало = {checkRepeatGrade(grade, studentsGrades)} студ.")
+    grade = grade + 0.50
+
+
+for student in range(1, countStudent + 1):
+    print(f'Середня оціка студента {student} с кроком 1 хв. = {averageGrades[student - 1]}')
+
+#with open('result.txt', 'w') as result:
+ #   result.write()
+
+
+
+
