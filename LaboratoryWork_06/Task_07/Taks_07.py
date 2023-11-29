d9df750780da8b51f4de727973ad512b16cfb16f
limanp <pasha.liman@ukr.net>
2023-11-29 12:34:10 +0200

    Laboratory_06 Task_07 done

--- LaboratoryWork_06/Task_07/Taks_07.py
+++ LaboratoryWork_06/Task_07/Taks_07.py
@@ -1,31 +1,46 @@
-def is_number(str):
+NUMBER_QUESTIONS = 20
+
+
+def is_number(string):
     try:
-        float(str)
+        float(string)
         return True
     except ValueError:
         return False
-def checkRepeatGrade(gradeCheck, grades):
+
+
+def check_repeat_grade(grade_check, grades):
     count = 0
-    for grade in grades:
-        if float(gradeCheck) == float(grade):
+    for gradeSt in grades:
+        if float(grade_check) == float(gradeSt):
             count = count + 1
     return count
 
 
+def separation():
+    return "========================================================================\n"
+
+
+# def replace_min_number(arr, number):
+#     index_min_num = arr.index(min(arr))
+#     arr[index_min_num] = number
+
 
 with open('marks.lab6.csv', encoding='UTF-8') as fileMarks:
-    studentsGrades = []
+    studentGrades = []
     countStudent = 0
     averageGrades = []
-    statsStudentResult = []
+    statsStudentResult = [0] * NUMBER_QUESTIONS
+    leadTimes = []
     for entry in fileMarks:
         student = entry.split(',', 4)
 
         resultTest = student[4].replace('\",\"', " ").replace('\"', "").replace(',-', ' ').split()
         grade = resultTest[0].replace(',', '.')
-        studentsGrades.append(grade)
+        resultTest.pop(0)
+        studentGrades.append(grade)
 
-        leadTime = student[3].replace(' хв ', '.').replace(' сек', '')
+        leadTimes.append(student[3].replace(' хв ', '.').replace(' сек', '').replace(' хв', ''))
 
         # Розрахунок середньої оцінки с кроком 1 хвилина
         averageGrades.append(float(grade) / (len(resultTest) - 1))
@@ -33,30 +48,53 @@ with open('marks.lab6.csv', encoding='UTF-8') as fileMarks:
         # Лічильник студентів
         countStudent += 1
 
-        i = 1
-        countCorrectAnswer = 0
+        i = 0
         while i < len(resultTest):
             if resultTest[i] == "0,50":
-                countCorrectAnswer = countCorrectAnswer + 1
+                statsStudentResult[i] = statsStudentResult[i] + 1
             i = i + 1
-        statsStudentResult.append(countCorrectAnswer)
-
 
 print(f'Кількість студентів що проходили тестування: {countStudent}\n')
-print(student)
-print(statsStudentResult)
 
 grade = 0
-while(grade <= 10):
-    print(f"Оцінку {grade} отримало = {checkRepeatGrade(grade, studentsGrades)} студ.")
+while grade <= 10:
+    print(f"Оцінку {grade} отримало = {check_repeat_grade(grade, studentGrades)} студ.")
     grade = grade + 0.50
-
+print(separation())
 
 for student in range(1, countStudent + 1):
-    print(f'Середня оціка студента {student} с кроком 1 хв. = {averageGrades[student - 1]}')
+    print(f'Середня оцінка студента {student} с кроком 1 хв. = {averageGrades[student - 1]:.2f}')
+print(separation())
 
-#with open('result.txt', 'w') as result:
- #   result.write()
+with open('result.txt', 'w', encoding='UTF-8') as resultFile:
+    resultFile.write("Статистика правильних відповідей:\n")
+    for question in range(NUMBER_QUESTIONS):
+        result = statsStudentResult[question] / countStudent * 100
+        resultFile.write(f"\tПитання {question + 1} = {result:.2f} %\n")
+
+    resultFile.write(separation())
+
+    resultFile.write("Статистика неправильних відповідей:\n")
+    for question in range(NUMBER_QUESTIONS):
+        result = 100 - statsStudentResult[question] / countStudent * 100
+        resultFile.write(f"\tПитання {question + 1} = {result:.2f} %\n")
+
+    fiveBestGrades = [8] * 5
+    gradesRatioTimes = []
+    i = 0
+    while i < len(studentGrades):
+        gradesRatioTimes.append(float(studentGrades[i]) / float(leadTimes[i]))
+        i = i + 1
+
+    resultFile.write(separation())
+    resultFile.write("Найкращі 5 оцінок в співвідношенні оцінка/час:\n")
+    i = 0
+    while i < 5:
+        student = gradesRatioTimes.index(max(gradesRatioTimes))
+        resultFile.write(f"\tСтудент {student + 1} : Оцінка = {studentGrades[student]}: Час виконання = {leadTimes[student]}\n")
+        del gradesRatioTimes[student]
+        i = i + 1
+
 
 
 
