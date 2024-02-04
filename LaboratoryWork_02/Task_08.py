c2a33763439364b43d3963e84ab3d6d9c7a56f32
limanp <pasha.liman@ukr.net>
2024-02-04 20:01:50 +0200

    update report

--- LaboratoryWork_02/Task_08.py
+++ LaboratoryWork_02/Task_08.py
@@ -6,8 +6,8 @@ numbers = [int(input()), int(input()), int(input())]
 count = 0
 
 i = 0
-while(i < len(numbers)):
-    if(numbers[i] > 0):
+while i < len(numbers):
+    if numbers[i] > 0:
         count += 1
     i += 1
 
